90fe60f123eac74f5f8bb95f60736644a64c6fa5
gaurav <gaurav@ggvaidya.com>
2014-05-23 00:24:57 -0600

    Rerun extraction, added a script to search by file name.

---
+++ collect-rdf-on.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Run this script to strip the headers and sort the
+# triples into alphabetical order, making comparisons
+# between them easier.
+
+for dir in */*; do 
+    for filename in $dir/*.ttl; do 
+        # echo Searching for $1.
+        url="<http://commons.dbpedia.org/resource/$1>"
+        found=`grep "$url" $filename`
+        echo ${found//$url/[$1]}
+        #grep "$url" $filename
+    done;
+done
